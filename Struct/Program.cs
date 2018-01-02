82a2122c240d5bc69bf0eab92cd5eab345256981
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-02 22:02:53 +1100

    Added Constructor

--- Struct/Program.cs
+++ Struct/Program.cs
@@ -4,4 +4,14 @@
     private int x, y;
     private AnotherClass myClass;
     private Days my Days;
+
+    //Constructor
+    public MyStruct(int a, int b, int c)
+    {
+        myClass = new AnotherClass();
+        myClass.number = a;
+        x = b;
+        y = c;
+        myDays = Days.Mon;
+    }
 }
